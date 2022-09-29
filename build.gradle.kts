af7288ed24f2d86e4835e4bdc0876a1b9354a4e5
SirBlobman <blobmansrealm@gmail.com>
2022-09-29 12:42:54 -0400

    Switch to gradle.

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    id("java")
+    id("distribution")
+}
+
+repositories {
+    mavenCentral()
+
+    maven {
+        name = "sirblobman-public"
+        url = uri("https://nexus.sirblobman.xyz/repository/public/")
+    }
+
+    maven {
+        name = "dv8tion"
+        url = uri("https://m2.dv8tion.net/releases/")
+    }
+}
+
+dependencies {
+    // JDA
+    implementation("net.dv8tion:JDA:5.0.0-alpha.20") {
+        exclude("club.minnced", "opus-java")
+    }
+
+    // Emoji Handler Fork
+    implementation("com.github.minndevelopment:emoji-java:6.1.0")
+
+    // Other Dependencies
+    implementation("com.github.oshi:oshi-core:6.2.2")
+    implementation("com.j2html:j2html:1.6.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("org.xerial:sqlite-jdbc:3.39.3.0")
+    implementation("org.yaml:snakeyaml:1.33")
+}
+
+distributions {
+    main {
+        contents {
+            into("/target") {
+                from(tasks.named("jar"))
+                from(configurations["runtimeClasspath"])
+            }
+        }
+    }
+}
+
+tasks {
+    val baseVersion = findProperty("version.base") ?: ""
+    val jenkinsBuildNumber = System.getenv("BUILD_NUMBER") ?: "Unknown"
+    val calculatedVersion = "$baseVersion-$jenkinsBuildNumber"
+
+    val mainClassName = findProperty("discord.bot.main") ?: "Main"
+
+    val manifestDependencies = configurations.runtimeClasspath.get().joinToString(" ") {
+            file -> file.name
+    }
+
+    named<Jar>("jar") {
+        manifest {
+            attributes["Main-Class"] = mainClassName
+            attributes["Class-Path"] = manifestDependencies
+        }
+
+        archiveFileName.set("SlimyBot-$calculatedVersion.jar")
+    }
+
+    named<Tar>("distTar") {
+        enabled = false
+    }
+
+    named<Zip>("distZip") {
+        archiveFileName.set("SlimyBot-$calculatedVersion.zip")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Slimy Bot"
